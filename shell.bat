dbaa5f481904c42284afd7042322a88b7817a83b
shan-commits2 <shanlimbernardo39@gmail.com>
2025-08-05 12:20:41 +0800

    Update shell.bat

--- shell.bat
+++ shell.bat
@@ -1,6 +1,12 @@
 @echo off
+chcp 65001 >nul
 title $ Sudo Shell CMD
 color 0A
+
+:: Enable QuickEdit mode automatically (optional but useful)
+reg add "HKCU\Console" /v QuickEdit /t REG_DWORD /d 1 /f >nul
+
+echo [*] Launching Shell CMD...
 cd /d "%~dp0"
 python main.py
 pause
